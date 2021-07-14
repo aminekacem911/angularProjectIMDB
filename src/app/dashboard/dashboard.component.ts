902ec3ee121e63726d85b26953971ea27df62aee
amine <amine.kacem1377@gmail.com>
2021-07-14 16:48:21 +0100

    add crud functional

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public contacts: any= [];
+  public total:string='';
+  public onecontact :any=[]
+  constructor(
+    private contactservice:ContactService) {}
+
 
   ngOnInit(): void {
+    //this.products = this.productService.getProducts();
+    this.contactservice.getallMsg().subscribe((res:any) =>{
+      this.contacts = res['hits']['hits'];
+      this.total = res['hits']['total'];
+      console.log(this.contacts);
+    });
+
   }
+  delete(id:any)
+  {
+    return this.contactservice.deletemsg(id).subscribe((res:any) =>{
+      window.location.reload();
+      console.log("done!");
+    });
+  }
+  findmsg(id:any)
+  {
+    // this.contactservice.getallMsg().subscribe((res:any) =>{
+    //   const data  = res['hits']['hits'];
+    //   //console.log("ok",data);
+    //   for(let con of data){
+    //     console.log(id);
+    //     console.log("yo",con['_id']);
+    //      if(con['_id'] === id){
+    //        this.onecontact = con ;
+    //        console.log("okkkk",this.onecontact)
+    //      }
+    //   }
+
+    //   //console.log(this.onecontact);
+    // });
+    console.log("id",id);
+    // this.onecontact = this.contactservice.getMsg(id);
+    // console.log(this.contactservice.getMsg(id));
+    this.contactservice.getMsg(id).subscribe((res:any) =>{
+      this.onecontact = res['_source'];
+
+      console.log(this.onecontact);
+    });
+    }
+
 
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Contact } from '../shared/models/contact';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
-
-
   private contact: Array<Contact> = [];
-  private header:HttpHeaders;
-  constructor(public http:HttpClient) {
+  private header: HttpHeaders;
+  constructor(public http: HttpClient) {
     this.header = new HttpHeaders();
-    this.header = this.header.append('Content-Type','application/json');
+    this.header = this.header.append('Content-Type', 'application/json');
   }
-  addContact(contact:any){
-    return this.http.post("/miniproject/contact/",contact,{headers:this.header});
+  addContact(contact: any) {
+    return this.http.post('/miniproject/contact/', contact, {
+      headers: this.header,
+    });
   }
-  getContactFromServer(){
-    return this.http.get("/miniproject/contact/_search",{headers:this.header});
+  getallMsg() {
+    return this.http.get('/miniproject/contact/_search', {
+      headers: this.header,
+    });
+  }
+  getMsg(id: any) {
+    return this.http.get('/miniproject/contact/' + id, {
+      headers: this.header,
+    });
+  }
+  deletemsg(id: any) {
+    return this.http.delete('/miniproject/contact/' + id, {
+      headers: this.header,
+    });
   }
 }
